67d0e3da16a4b9c5ca2fbe41e691304aa85c2d39
Halil İbrahim Cam <76750950+Halil-Cam@users.noreply.github.com>
2022-07-22 14:35:38 +0300

    Update zar.py

--- zar.py
+++ zar.py
@@ -1,5 +1,5 @@
 from random import randint
-zar = randint(0,6)
+zar = randint(1,6)
 print(f"zar = {zar}")
 
 sayac = 0
@@ -7,7 +7,7 @@ sayac = 0
 while sayac <5:
     girdi = int(input("Devam etmek için 1 e bas = "))
     if girdi == 1:
-        zar = randint(0, 6)
+        zar = randint(1, 6)
         print(f"zar = {zar}")
         sayac+=1
         if sayac==5:
